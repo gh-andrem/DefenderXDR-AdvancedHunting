76a5dcdf2344d4382c04caace7874c340280a477
Andre <85677225+gh-andrem@users.noreply.github.com>
2025-07-24 10:59:13 +0200

    Update MDE-PS-MDAV-Update-Status.ps1

--- Scripts/MDE-PS-MDAV-Update-Status.ps1
+++ Scripts/MDE-PS-MDAV-Update-Status.ps1
@@ -1,6 +1,11 @@
-#name: MDAV Update Status
+#name: MDAV Signature Update Status
+#updated: 7/24/2025
 #description:
-#This PS script returns the most recent 15 Defender update events (event ID 2000) on any Windows device to detect issues with the signature update cycle/process.
+#This script returns the most recent Defender for Antivirus signature update events (event ID 2000), either full or delta updates.
+#It might be helpful for troubleshooting issues related to signature update cycle/process.
 
-$LastMDAVUpdates = Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; id=2000} -MaxEvents 15 | Select-Object TimeCreated, @{name="CurrentSignatureVersion"; expression={ $_.Properties[2].value }}, @{name="PreviousSignatureVersion"; expression={ $_.Properties[3].value }}, @{name="UpdateType"; expression={ $_.Properties[13].value }}
-$LastMDAVUpdates | Sort-Object -Property CurrentSignatureVersion -Unique | Sort-Object -Property TimeCreated
+$LastMDAVSigs = Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; id=2000} -MaxEvents 25 | Select-Object TimeCreated,
+    @{name="CurrentSignatureVersion"; expression={ $_.Properties[2].value }},
+    @{name="PreviousSignatureVersion"; expression={ $_.Properties[3].value }},
+    @{name="UpdateType"; expression={ $_.Properties[13].value }}
+$LastMDAVSigs | Sort-Object -Property CurrentSignatureVersion -Unique | Sort-Object -Property TimeCreated
