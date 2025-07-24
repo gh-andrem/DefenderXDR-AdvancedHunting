2a9af0ee2cfb8bcd109af0187b637400bc8d3fef
Andre <85677225+gh-andrem@users.noreply.github.com>
2025-07-24 11:01:48 +0200

    Create MDE-PS-MDAV-Platform-Update-Status.ps1

---
+++ Scripts/MDE-PS-MDAV-Platform-Update-Status.ps1
@@ -0,0 +1,6 @@
+#name: MDAV Platform Update Status
+#updated: 7/24/2025
+#description:
+#This script returns the most recent Defender for Antivirus platform update events (event ID 2014).
+
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; id=2014} -MaxEvents 2000 | Select-Object TimeCreated, @{name="PlatformVersion"; expression={ $_.Properties[1].value }}
